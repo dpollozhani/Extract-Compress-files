99f5828331623da6a1afaff0eca5a0b5c2af1905
dpollozhani <38571830+dpollozhani@users.noreply.github.com>
2020-04-11 00:43:32 +0200

    Changed paths in .bat files

--- Unzip_files.bat
+++ Unzip_files.bat
@@ -1,4 +1,4 @@
 REM Runs python script for unzipping files in given directory/file path
-python.exe "C:\Users\drilo\Python\Own code\Unzip Files\Unzip_files.py"
+python.exe "C:\Users\drilo\Python\Own code\Extract-Compress-files\Unzip_files.py"
 Echo Completed.
 Pause
